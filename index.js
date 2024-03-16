7aa8c2c5f0634364891186577edd0798a7f1fd66
OscarSanti29 <osantiesteban69@hotmail.com>
2024-03-15 21:03:16 -0500

    updated file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const names = ["Alice", "Bob", "Carol","Carlos", "Amy"];
+const names = ["Alice", "Bob", "Carol", "Carlos", "Amy"];
 
-const occupations = ["Writer","teacher","Programmer","Baker","Plumber"];
+const occupations = ["Writer", "Teacher", "Programmer", "Baker", "Plumber"];
 
 const prices = [20, 30, 40, 50, 60, 70, 80];
 
@@ -8,38 +8,60 @@ const freelancers = [];
 
 const maxlancers = 7;
 
+const maxavearge = 7;
+
 const addrandomlancerIntervalId = setInterval(addrandomlancer, 1000);
 const loadaverage = setInterval(averageprice, 1000);
 
-function render(){
+function render() {
     const lancers = document.querySelector("#lancers");
-    const lancerElements = freelancers.map((lancer)=>{
+    
+    lancers.innerHTML = '';
+    
+    const lancerElements = freelancers.map((lancer) => {
         const element = document.createElement("li");
-        element.classList.add(lancer.name,lancer.occupation,lancer.price);
+        
+        element.textContent = `${lancer.name} - ${lancer.occupation} - $${lancer.price}`;
+        
         return element;
     });
-    lancers.replaceChildren(...lancerElements);
+    lancers.append(...lancerElements);
+
 };
 
-function addrandomlancer(){
-    const name = names[Math.floor(Math.random()*names.length)];
 
-    const occupation = occupations[Math.floor(Math.random()*occupations.length)];
-
-    const price = prices[Math.floor(Math.random()*prices.length)]; 
-
-    freelancers.push({name, occupation, price});
-
+function addrandomlancer() {
+    const name = names[Math.floor(Math.random() * names.length)];
+    
+    const occupation = occupations[Math.floor(Math.random() * occupations.length)];
+    
+    const price = prices[Math.floor(Math.random() * prices.length)];
+    
+    freelancers.push({ name, occupation, price });
     render();
-
-    if(freelancers.length >= maxlancers){
+    if (freelancers.length >= maxlancers) {
         clearInterval(addrandomlancerIntervalId);
+    }};
+
+function averageprice() {
+    const sum = freelancers.reduce((total, currentValue) => total + currentValue.price, 0);
+    
+    const average = sum / freelancers.length;
+    
+    console.log("Average price:", average);
+
+    if(freelancers.length >= maxavearge){
+        clearInterval(loadaverage);
     }
-
 };
 
-function averageprice(){
-    const sum = freelancers.reduce((total, currentvalue) => total + currentvalue.price, 0) / freelancers.length;
+document.addEventListener("DOMContentLoaded", function(){
 
-    return sum
-};
+    const div = document.querySelector("div");
+
+    const p = document.createElement("p");
+
+    p.textContent= `${averageprice()}`;
+
+    div.append(p);
+});
